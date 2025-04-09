c8d9d41093e311b5321e46ed32e0a56c9fdda1f0
Radek Wójcik <radekw04@gmail.com>
2025-04-09 18:27:00 +0200

    Przetestowanie działania tablicy

--- MiniProjekt1 TablicaDynamiczna/MiniProjekt1 TablicaDynamiczna.cpp
+++ MiniProjekt1 TablicaDynamiczna/MiniProjekt1 TablicaDynamiczna.cpp
@@ -5,7 +5,14 @@ using namespace std;
 
 int main() {
     ArrayList list(5);
-
-
+    for (int i = 0; i < 10000; ++i) {
+        list.push_back(i);
+        cout << "Rozmiar: " << list.size() << "|| Pojemnosc: " << list.getCapacity() << endl;
+    }
+    cout << "Zawartosc tablicy: ";
+    for (int i = 0; i < list.size(); ++i) {
+        cout << list[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
