23f9ea6c082511e8e7a099e48109a2bd635b7611
Radek Wójcik <radekw04@gmail.com>
2025-04-09 18:11:51 +0200

    Stworzenie metody push oraz resize

--- MiniProjekt1 TablicaDynamiczna/ArrayList.cpp
+++ MiniProjekt1 TablicaDynamiczna/ArrayList.cpp
@@ -9,3 +9,20 @@ ArrayList::ArrayList(int initCapacity)
 ArrayList::~ArrayList() {
     delete[] array;                                    
 }
+
+void ArrayList::resize() {
+    capacity *= 2;
+    int* newArray = new int[capacity];
+    for (int i = 0; i < length; ++i) {
+        newArray[i] = array[i];
+    }
+    delete[] array;
+    array = newArray;
+}
+
+void ArrayList::push_back(const int& value) {           
+    if (length == capacity) {
+        resize();
+    }
+    array[length++] = value;
+}
--- MiniProjekt1 TablicaDynamiczna/ArrayList.h
+++ MiniProjekt1 TablicaDynamiczna/ArrayList.h
@@ -7,9 +7,12 @@ private:
     int capacity;    
     int length;      
 
+    void resize();
 public:
     ArrayList(int initCapacity = 10);  
     ~ArrayList();
+
+    void push_back(const int& value);
 };
 
 #endif // ARRAYLIST_H
